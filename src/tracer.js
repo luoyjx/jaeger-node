a3d4b6d55b983258bf7a7a5c4412b47e897b371c
luoyjx <yanjixiong@shimo.im>
2018-02-06 21:39:53 +0800

    perf: sender and instruments configuration

--- src/tracer.js
+++ src/tracer.js
@@ -10,17 +10,21 @@ const UDPSender = require('shimo-jaeger-client/dist/src/reporters/udp_sender').d
 class Tracer {
   constructor ({
     serviceName,
-    sampler = new jaeger.RateLimitingSampler(1),
-    reporter = new jaeger.RemoteReporter(new UDPSender()),
+    sampler,
+    reporter,
     options = {}
   }) {
     if (!serviceName) {
       throw new Error('serviceName is required')
     }
 
+    sampler = sampler || new jaeger.RateLimitingSampler(1)
+    reporter = reporter || new jaeger.RemoteReporter(new UDPSender(Object.assign({}, options.sender)))
+
     this._tracer = new jaeger.Tracer(serviceName, reporter, sampler, options)
     this.instrument = new Instrument({
-      tracers: [this._tracer]
+      tracers: [this._tracer],
+      enables: options.enables
     })
   }
 }
